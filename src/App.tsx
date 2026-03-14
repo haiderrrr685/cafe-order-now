a1440ca464af0eccf64260bfb3c06162440c3a3c
haiderrrr685 <mohdhaider78690@gmail.com>
2026-03-14 17:14:02 +0530

    Add Kitchen route to App componentAdd kitchen route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,25 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import Kitchen from "./pages/Kitchen.tsx";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+    <QueryClientProvider client={queryClient}>
+          <TooltipProvider>
+                <Toaster />
+                <Sonner />
+                <BrowserRouter>
+                        <Routes>
+                                  <Route path="/" element={<Index />} />
+                                  <Route path="/kitchen" element={<Kitchen />} />
+                                  <Route path="*" element={<NotFound />} />
+                        </Routes>Routes>
+                </BrowserRouter>BrowserRouter>
+          </TooltipProvider>TooltipProvider>
+    </QueryClientProvider>QueryClientProvider>
+  );
 
 export default App;
+</TooltipProvider>
